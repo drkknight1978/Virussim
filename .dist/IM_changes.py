b0eb09f29e3b6f90c97e1779d0ad4fa78b58ed66
Colin Mclaren <colin.mclaren7878@gmail.com>
2020-04-16 16:25:49 +0100

    Population class created.

--- .dist/IM_changes.py
+++ .dist/IM_changes.py
@@ -1,6 +1,6 @@
 # TO DO's
-# 1. Create a Technique for adding a border DONE 14/4/20
-# 2. Create a psuedoname function. DONE 14/4/20
+# 1. Create a Technique for adding a border DONE 14/04/2020
+# 2. Create a psuedoname function.
 # 3. Implement a population class and the current version is elegant.
 # 4. Implement a world class to create multiple sims
 # 5. Speed Up, Multi-threading, implement through an image and numpy arrage?
@@ -22,19 +22,27 @@ X_BORDER_MAX, Y_BORDER_MAX = (X_RESOLUTION - RIGHT_BORDER, Y_RESOLUTION - TOP_BO
 X_BORDER_MIN, Y_BORDER_MIN = (LEFT_BORDER , BOTTOM_BORDER)
 
 #Civilisation aspects
-
+POPSIZE = 1000                 # Population size
+MAXSPEED = 2                # Maximum movement speed of victims
+HP_MAX = 0                  # Health Points the amount of times you pass an infected victim before getting disease
+DECISION_RANGE = 10         # Maximum number of moves before a victim might change direction.
+VICTIM_RADIUS = 0          # Victims are represented as circles this is thier radius.
 
 #State parameters - Defines the colours of the various states of the victims.
 HEALTHY = [255, 255, 255]   # Healthy are White 
 INFECTED = [255, 0, 0]      # Infected are Red
-CURED = [0, 255, 0]     # Victimes no longer infected/ infectable
+CURED = [0, 255, 0]         # Victimes no longer infected/ infectable
 
 #Virus aspects
 INFECTION_RADIUS = 2        # Distance from the surface of the victim another victim can be affetced.
-RECOVERY_TIME = 100         # Amount of ticks before a victim is no longer infectable (dead / immune)
+RECOVERY_TIME = 300         # Amount of ticks before a victim is no longer infectable (dead / immune)
 
 #World aspects
 WORLDCLOCK = 0              # The worlds universal clock (ticks in __main__)
+
+#a quick and dirty way to display text on the screen. to be developed.
+def text_display(inpsurface, txtstring, x,y):
+    pass
 
 def listtostr(list_inp):
     #A little function to convert a list to a string tries to create a line for a csv file.
@@ -49,41 +57,7 @@ def psuedoName():
     for I in range(5):
         psName += chr(int(random()*26)+65)
     return (psName)
-
-class population:
-    def __init__(self):
-        # popsize =Population size
-        # max_speed = Maximum movement speed of victims
-        # HP_max = Health Points the amount of times you pass an infected victim before getting disease
-        # decision_range =Maximum number of moves before a victim might change direction.
-        # victim_radius =Victims are represented as circles this is thier radius.
-        self.group = [] #list that contains victim objects.
-
-    
-    def generate_population(self, popsize = 200, max_speed = 1, decision_range = 1, HP_max = 0):
-            #Generate the initial population
-        for _ in range(popsize):
-            X = int(random() * (X_RESOLUTION - (LEFT_BORDER + RIGHT_BORDER))) + LEFT_BORDER ####### 
-            Y = int(random() * (Y_RESOLUTION - (TOP_BORDER + BOTTOM_BORDER))) + BOTTOM_BORDER #######
-            DY = int(random() * (max_speed + 1 * 2)) - max_speed
-            DX = int(random() * (max_speed + 1 * 2)) - max_speed
-            self.group.append(victim(X, Y, DX, DY, max_speed, decision_range , HP_max ))
-
-    def story(self):
-        #returns a poll of the current H/I/C population.
-        sort_info = self.group
-        H = 0
-        I = 0
-        C = 0
-        for v in sort_info:
-            if v.health == 'H':
-                H += 1
-            if v.health == 'I':
-                I += 1
-            if v.health == 'C':
-                C += 1
-        return ([WORLDCLOCK, H, I, C])
-
+        
 
 class victim:
     '''This is the victim class.
@@ -91,21 +65,19 @@ class victim:
     Health status (H)ealthy, (I)fected and (C)ured, victim size, 
     the maximum distance a victim will go before changeing direction (changedir), time according to the WORLDCLOCK
     when the victim get infected''' 
+    population = []
     victims = 0
 
-    def __init__(self, posX, posY, dirX, dirY, max_speed, decision_range = 1, HP_max = 0, health = 'H', HP = 0, radius = 1, infectDate= 0):
+    def __init__(self, posX, posY, dirX, dirY, health = 'H', HP = 0, radius = VICTIM_RADIUS, changedir = 0, infectDate= 0):
         self.posX = posX
         self.posY = posY
         self.dirX = dirX
         self.dirY = dirY
-        self.max_speed = max_speed
-        self.changedir = int(random()*decision_range)
-        self.HP_max = HP_max
         self.health = health
         self.HP = HP
         self.radius = radius
+        self.changedir = int(random()*DECISION_RANGE)
         self.infectDate = infectDate
- 
 
         self.check_state = { #--------------- is this the right place?
         'H' : HEALTHY,
@@ -118,7 +90,7 @@ class victim:
 
     def draw(self, scrn):
         # Pass the pygame screen object and map a victim to it.
-        pygame.draw.circle(scrn, self.check_state.get(self.health) , (self.posX, self.posY), self.radius)
+        pygame.draw.circle(scrn, self.check_state.get(self.health), (self.posX, self.posY), self.radius)
 
     def move(self):
         #Movement method.
@@ -128,7 +100,7 @@ class victim:
         if dice == 1: 
             # randomly changes direcion in the x, y, xy directions.
             dice = int(random() * 3)
-            spd_change = int((random() - 0.5) * (self.max_speed * 2))
+            spd_change = int((random() - 0.5) * (MAXSPEED * 2))
             if dice == 0:
                 self.dirX = spd_change
             if dice == 1:
@@ -153,11 +125,10 @@ class victim:
         if self.posY < Y_BORDER_MIN:
             self.posY = Y_BORDER_MAX + (self.posY - Y_BORDER_MIN)
 
-
     def infected(self):
         #Checks if the victim is already infected. If so, cycle through the other victimss
         if self.health == 'I':
-            for v in britain.group:
+            for v in victim.population:
                 # Check that the victim can be infected. If so, check the distance between this one
                 # and the examined one if less than infection radius and reached the maximum Health Point
                 # infect the victim and register the time of infection, or if still within the infection
@@ -167,7 +138,7 @@ class victim:
                     distY = v.posY - self.posY
                     distR = int(abs(sqrt(abs(distX ^ 2) + abs(distY ^ 2))))
                     if distR < INFECTION_RADIUS:
-                        if v.HP == self.HP_max:
+                        if v.HP == HP_MAX:
                             v.health = 'I'
                             v.infectDate = WORLDCLOCK
                             v.HP += 1
@@ -179,6 +150,21 @@ class victim:
         if self.health == 'I' and RECOVERY_TIME <= (WORLDCLOCK - self.infectDate):
             self.health = 'C'
     
+    @classmethod
+    def story(cls):
+        #returns a poll of the current H/I/C population.
+        sort_info = cls.population
+        H = 0
+        I = 0
+        C = 0
+        for v in sort_info:
+            if v.health == 'H':
+                H += 1
+            if v.health == 'I':
+                I += 1
+            if v.health == 'C':
+                C += 1
+        return ([WORLDCLOCK, H, I, C])
 
        
 
@@ -188,22 +174,26 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode([X_RESOLUTION, Y_RESOLUTION])
     pygame.display.set_caption("Virus World")
 
-    #create a population
-    britain = population()
-    britain.generate_population()
-
+    #Generate the initial population
+    for _ in range(POPSIZE):
+        X = int(random() * (X_RESOLUTION - (LEFT_BORDER + RIGHT_BORDER))) + LEFT_BORDER ####### 
+        Y = int(random() * (Y_RESOLUTION - (TOP_BORDER + BOTTOM_BORDER))) + BOTTOM_BORDER #######
+        DY = int(random() * (MAXSPEED + 1 * 2)) - MAXSPEED
+        DX = int(random() * (MAXSPEED + 1 * 2)) - MAXSPEED
+        victim.population.append(victim(X, Y, DX, DY))
+        print(X)
     #Create one infected victim to drop into the Simulation
-    britain.group[0].health = 'I'
+    victim.population[0].health = 'I'
 
     #f = open('small4.csv','w') # open a file to record data my be NEEDS MODIFYING
 
     #Initialise the running variables
     record = [] 
-    record.append(britain.story())
+    record.append(victim.story())
     running = True
     
     while  running != False:
-        for v in britain.group:
+        for v in victim.population:
             #Itterate through each victim and Move, check collision, check infection, check recovered and draw.
             v.move()
             v.collision()
@@ -218,20 +208,14 @@ if __name__ == "__main__":
         # Tick the clock forward.
         WORLDCLOCK += 1
 
-        info = britain.story()
-
-        print((info[1]/250), (info[2]/250), (info[3]/250))
-
+        record.append(victim.story())
 
         #Saves a file and debugging
         #f.write(listtostr (record))
-        #print(listtostr (record)) This prints the population.
+        #print(listtostr (record) + '/n') #This prints the population.
 
         #Exit Points
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-
-
 #f.close()
